a870bfa9550f22d8ed9a5f6fd6727cbb39d5210e
YadhuCU <yadhuakku003@gmail.com>
2024-02-06 17:39:22 +0530

    made upload folder public

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router); // always after cors
+app.use("/uploads", express.static("./uploads"));
 
 app.listen(PORT, () => {
   console.log("Sever is runnig at " + PORT);
